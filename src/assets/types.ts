70068a9d32d2865e2fcd3452e513b51f9150f35f
sdubourg <sdubourg@tenable.com>
2022-06-23 02:16:04 +0200

    WIP - Add wallpaper to desktop screen

--- src/assets/types.ts
+++ src/assets/types.ts
@@ -1,4 +1,4 @@
-export type Project = 'pokedex' | 'join' | 'sharky' | 'portfolio' | 'jelly' | 'logo';
+export type Project = 'pokedex' | 'join' | 'sharky' | 'portfolio' | 'jelly' | 'logo' | 'buche';
 
 export interface Asset {
   pictures: Record<Project, string>;
