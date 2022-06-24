e7874a947dac26d3190933395b9171d15858c494
Jénaïc Cambré <me@jenaiccambre.com>
2022-06-24 23:21:14 +0200

    add server express + socket io + tmi.js

---
+++ server/src/twitch.js
@@ -0,0 +1,18 @@
+const tmi = require("tmi.js");
+
+const { TWITCH_USERNAME } = process.env;
+
+module.exports = (socket) => {
+  const client = new tmi.Client({
+    channels: [TWITCH_USERNAME],
+  });
+
+  client.connect();
+  client.on("message", (channel, tags, message, self) => {
+    console.log("messages");
+    socket.emit("new-message", {
+      username: tags.username,
+      message,
+    });
+  });
+};
